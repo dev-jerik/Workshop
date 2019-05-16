dbac7f0d9cdbc89c859308585b06782548dda239
jgbeltran <jerik@courtesypoint.com>
2019-05-16 14:05:18 +0800

    Issue#102
    ONgoing demosdfdsafdasf

--- HelloWorld.java
+++ HelloWorld.java
@@ -17,10 +17,10 @@
  ******************************************************************************/
 
 public class HelloWorld {
-
+asdfas
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello, World Cpti");
     }
 
 }
